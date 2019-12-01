8a6a8304c56e31e98fb5b443c702672ddf58ffd4
Bjorn Lindi <blindij@users.noreply.github.com>
2019-12-01 16:46:57 +0100

    Move class Franc methods to own file.

---
+++ src/Franc.cpp
@@ -0,0 +1,30 @@
+// File: Franc.cpp
+//
+// Move Franc Class methods in own file
+#include <TestDrivenDevbyExample/lib.hpp>
+
+Franc::Franc(int value) : Money(value){}
+
+
+bool Franc::equal(const Money &rhs) const {
+   // we know the types are equal, so the cast won't throw
+   auto r = dynamic_cast<const Franc&>(rhs);
+   return (this->value == r.value);
+}
+
+int Franc::amount(){
+      return value;
+}
+
+Franc Franc::operator*(int factor) const{
+   Franc t = *this;
+   t.value *= factor;
+   return t;
+}
+
+Franc operator*(int factor, const Franc & d) {
+   Franc t = d;
+   t.value *= factor;
+   return t;
+}
+
--- src/lib.cpp
+++ src/lib.cpp
@@ -39,29 +39,3 @@ Dollar operator*(int factor, const Dollar & d) {
    t.value *= factor;
    return t;
 }
-
-Franc::Franc(int value) : Money(value){}
-
-
-bool Franc::equal(const Money &rhs) const {
-   // we know the types are equal, so the cast won't throw
-   auto r = dynamic_cast<const Franc&>(rhs);
-   return (this->value == r.value);
-}
-
-int Franc::amount(){
-      return value;
-}
-
-Franc Franc::operator*(int factor) const{
-   Franc t = *this;
-   t.value *= factor;
-   return t;
-}
-
-Franc operator*(int factor, const Franc & d) {
-   Franc t = d;
-   t.value *= factor;
-   return t;
-}
-
